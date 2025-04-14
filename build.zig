5b46a67c8ec9682c0f6b03d11cd31d1cb1e70b61
44mira <legolaslada@gmail.com>
2025-04-15 01:54:53 +0800

    refactor: rename internal and rewrite build unit test instruction

--- build.zig
+++ build.zig
@@ -1,11 +1,12 @@
 const std = @import("std");
+const ArrayList = std.ArrayList;
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_mod = b.createModule(.{
-        .root_source_file = b.path("src/internal.zig"),
+    const attempt_mod = b.createModule(.{
+        .root_source_file = b.path("src/attempt.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -15,12 +16,12 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    exe_mod.addImport("internal_lib", lib_mod);
+    exe_mod.addImport("attempt", attempt_mod);
 
-    const lib = b.addLibrary(.{
+    const attempt_lib = b.addLibrary(.{
         .linkage = .static,
-        .name = "internal_lib",
-        .root_module = lib_mod,
+        .name = "attempt",
+        .root_module = attempt_mod,
     });
 
     const exe = b.addExecutable(.{
@@ -28,7 +29,7 @@ pub fn build(b: *std.Build) void {
         .root_module = exe_mod,
     });
 
-    b.installArtifact(lib);
+    b.installArtifact(attempt_lib);
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
@@ -41,18 +42,16 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
-    });
-
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    const modules = [_]*std.Build.Module{ attempt_mod, exe_mod };
 
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
+    for (modules) |module| {
+        const unit_tests = b.addTest(.{
+            .root_module = module,
+        });
+
+        const run_unit_tests = b.addRunArtifact(unit_tests);
+
+        test_step.dependOn(&run_unit_tests.step);
+    }
 }
